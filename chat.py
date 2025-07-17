97b77b1272112d063830c1a5a3d6b6d1887163ba
Clement Chong <119944151+clementtech@users.noreply.github.com>
2025-07-17 12:28:51 +0800

    added some response that i can thought of

--- chat.py
+++ chat.py
@@ -1,5 +1,17 @@
 response = str(input("You: "))
 
 match response.lower():
-    case "hello":
+    case "hello" | "hi":
         print("Hello, how can I help you?")
+    case "bye" | "goodbye":
+        print("Goodbye! Have a nice day.")
+    case "thanks" | "thank you":
+        print("You're welcome! Feel free to let me know if you need any other help!")
+    case "how are you":
+        print("I'm just a program, but I'm doing well!")
+    case "fuck you":
+        print("Sorry, please do not use inappropriate languages here!")
+    case "api" | "what is your api key":
+        print("Nice try in jailbreak me!")
+    case _:
+        print("Sorry, I didn't understand that.")
